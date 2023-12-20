5d1371aef75e6ea800053da0c41978ff7cd7c6bf
Elena <helenochka4@gmail.com>
2023-12-20 11:57:34 +0100

    task

--- lesson11.12/test.sql
+++ lesson11.12/test.sql
@@ -1,5 +1,8 @@
 https://western-appeal-39b.notion.site/GenTech-Summary-Dec-11-2023-b46098a7a8484e28a0c0320f4744bde9
 
+Список всех конспектов
+ https://western-appeal-39b.notion.site/GenTech-190923-m-be-m-fe-m-qa-ed7e7a76535646e0a5fe8daf9f27c866
+
 
 
 
--- lesson20.12/lesson4.sql
+++ lesson20.12/lesson4.sql
@@ -1,5 +1,9 @@
 https://www.w3schools.com/mysql/trymysql.asp?filename=trysql_select_all
 
+https://western-appeal-39b.notion.site/GenTech-3-Dec-20-2023-19a36afcea9b4545846b95cf19d12218
+
+Список всех конспектов
+ https://western-appeal-39b.notion.site/GenTech-190923-m-be-m-fe-m-qa-ed7e7a76535646e0a5fe8daf9f27c866
 
 -- Operator LIKE
 
@@ -29,5 +33,107 @@ https://www.w3schools.com/mysql/trymysql.asp?filename=trysql_select_all
 
 оператор,который помогает связать страницы -"JOIN"
 
+ON -по какому полю
+
 SELECT * FROM Products
 JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+
+
+SELECT
+	Orders.OrderID,
+  Customers.CustomerName
+FROM Orders
+
+JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+
+
+соединить таблицы 
+
+SELECT *
+FROM Orders
+
+JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+
+Задача. Вывести данные о товарах
+(проекция: название_товара, цена_товара, название_категории)
+
+SELECT
+  Products.ProductName,
+  Products.Price,
+  Categories.CategoryName
+FROM Products
+JOIN Categories ON Products.CategoryID = Categories.CategoryID
+
+
+**Задача. Вывести данные о заказах**
+
+(проекция: `номер_заказа`, `имя_менеджера`, `фамилия_менеджера`)
+
+SELECT
+  Orders.OrderID,
+  Employees.FirstName,
+  Employees.LastName
+FROM Orders
+JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+
+
+Пример. Применение псевдонимов в JOIN
+SELECT
+    P.ProductName,
+    P.Price,
+    C.CategoryName
+FROM Products AS P
+
+JOIN Categories AS C ON P.CategoryID = C.CategoryID
+
+
+**Задача. Вывести данные о заказах клиентов из `Germany`**
+
+(проекция: `номер_заказа`, `имя_клиента`, `контактное_имя_клиента`)
+
+SELECT
+  Orders.OrderID,
+  Customers.CustomerName,
+  Customers.ContactName
+FROM Orders
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE
+  Customers.Country = "Germany"
+
+**Задача. Вывести данные о товарах с ценой от `10` до `150` EUR, которые были поставлены из `Japan`, проекция:**
+
+- `название_товара`
+- `цена_товара`
+- `цена_со_скидкой_15_процентов`
+- `название_поставщика`
+
+SELECT
+  Products.ProductName,
+  Products.Price,
+  Products.Price * .85,
+  Suppliers.SupplierName
+FROM Products
+JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+WHERE
+  Products.Price BETWEEN 10 AND 150
+  AND
+  Suppliers.Country = "Japan"
+
+
+    //--- 2 variant--------------------------
+  SELECT
+		Products.ProductName,
+    Products.Price AS Price_full,
+    Products.Price * .85 AS Price_discount_15_perc,
+    Suppliers.SupplierName
+
+FROM Products
+
+JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+
+WHERE
+	Suppliers.Country='Japan'
+	AND
+	Products.Price BETWEEN 10 AND 150
