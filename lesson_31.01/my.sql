62321f25f142b7d6150728dfee074d3f2245bd36
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-31 12:38:13 +0100

    worckBanche

---
+++ lesson_31.01/my.sql
@@ -0,0 +1,16 @@
+https://western-appeal-39b.notion.site/GenTech-190923-m-be-m-fe-m-qa-ed7e7a76535646e0a5fe8daf9f27c866
+
+
+https://dbdiagram.io/d/online_chessmate_17jan2024-65a796d9ac844320ae18dbbe
+
+https://www.w3schools.com/mysql/trymysql.asp?filename=trysql_select_all
+
+--Как создать описанную базу данных
+
+https://western-appeal-39b.notion.site/GenTech-7-Jan-31-2024-6266862efcb64786a29329d83fa70f45
+
+https://sqliteonline.com/
+
+
+--сегодняшний урок 
+https://western-appeal-39b.notion.site/GenTech-7-Jan-31-2024-6266862efcb64786a29329d83fa70f45
